ee71498cef40f35ee0cd1b0db71708d3db59f7e0
Jamie Pittak <charrawr456@gmail.com>
2018-05-11 23:27:52 -0700

    add control to bottom of app

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey=>{
+  let transformedIngredients = Object.keys(props.ingredients).map(ingredientKey=>{
     return [...Array(props.ingredients[ingredientKey])].map((_, i)=>{
       return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>;
     });
-  });
+  })
+  .reduce((array, element) =>{
+    return array.concat(element)
+  },[]);
+
+  if (transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
