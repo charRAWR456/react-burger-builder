6946a8c57233c6c09e468b49501cec846e84edcd
Jamie Pittak <charrawr456@gmail.com>
2018-05-12 22:36:04 -0700

    add modal that shows upon checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Aux from "../../hoc/Aux";
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -19,11 +21,11 @@ class BurgerBuilder extends Component{
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   }
 
   updatePurchaseState(ingredients) {
-  
     const sum = Object.keys(ingredients)
       .map(ingredientKey => {
         return ingredients[ingredientKey]
@@ -32,6 +34,10 @@ class BurgerBuilder extends Component{
         return sum + el;
       }, 0);
       this.setState({purchasable: sum > 0})
+  }
+
+  purchaseHandler = () =>{
+    this.setState({purchasing: true});
   }
 
   addIngredientHandler = (type) => {
@@ -75,6 +81,10 @@ class BurgerBuilder extends Component{
 
     return(
       <Aux>
+        <Modal show={this.state.purchasing}>
+          <OrderSummary
+          ingredients={this.state.ingredients} />
+        </Modal>
         <Burger
           ingredients={this.state.ingredients}/>
         <BuildControls
@@ -82,7 +92,8 @@ class BurgerBuilder extends Component{
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
-          purchasable={this.state.purchasable}/>
+          purchasable={this.state.purchasable}
+          orderNow={this.purchaseHandler}/>
       </Aux>
     );
   }
